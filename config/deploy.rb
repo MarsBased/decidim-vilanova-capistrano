f5431c1e440c7b96f6adf9db566b6e2e5375ac06
Xavier <xavier.redo@marsbased.com>
2017-03-15 16:28:19 +0100

    Downgrade capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.8.0'
+lock '3.6'
 
 set :application, 'decidim-vilanova'
 set :repo_url, 'https://github.com/MarsBased/decidim-vilanova.git'
